c41eba4c552d609ad403a0d41edc98458e625ced
Veer Modi <veer.modi.cg@gmail.com>
2024-12-24 11:01:48 +0000

    Sync LeetCode submission - Remove Element (cpp)

---
+++ leetcode/problems/remove_element/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        int i, j = 0;
+        int numsSize= nums.size();
+
+        for (i = 0; i < numsSize; i++) {
+            if (nums[i] == val) {
+                count++;
+            } else {
+                nums[i - count] = nums[i];
+            }
+        }
+        return numsSize - count;
+    }
+};
