258417745b2e72818dfc9b492f636031a10c7c8d
Veer Modi <veer.modi.cg@gmail.com>
2025-02-02 15:52:46 +0000

    Sync LeetCode submission - Largest Substring Between Two Equal Characters (cpp)

---
+++ leetcode/problems/largest_substring_between_two_equal_characters/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+         int maxLength = -1;
+    
+    for (int i = 0; i < s.length(); i++) {
+        for (int j = s.length() - 1; j > i; j--) {
+            if (s[i] == s[j]) {  
+                int length = j - i - 1;
+                maxLength = max(maxLength, length);
+                break;
+            }
+        }
+    }
+    
+    return maxLength;
+    }
+};
