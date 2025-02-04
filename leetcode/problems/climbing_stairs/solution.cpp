d18c6ce9e446b46d03f68a070b50e18cfa6b5278
Veer Modi <veer.modi.cg@gmail.com>
2025-02-04 11:15:15 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- leetcode/problems/climbing_stairs/solution.cpp
+++ leetcode/problems/climbing_stairs/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2)
+        if (n <= 2) {
             return n;
-
-        int num1 = 1, num2 = 2;
-        for (int i = 3; i <= n; i++) {
-            int num3 = num1 + num2;
-            num1 = num2;
-            num2 = num3;
+        } else {
+            int num1 = 1, num2 = 2, temp;
+            for (int i = 3; i <= n; i++) {
+                temp = num1 + num2;
+                num1 = num2;
+                num2 = temp;
+            }
+            return num2;
         }
-
-        return num2;
     }
 };
