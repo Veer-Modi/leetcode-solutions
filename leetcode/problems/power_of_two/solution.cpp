730f9bfb9ff90c85850930bd67cc4e98f70527ed
Veer Modi <veer.modi.cg@gmail.com>
2025-09-24 10:49:03 +0000

    Sync LeetCode submission - Power of Two (cpp)

--- leetcode/problems/power_of_two/solution.cpp
+++ leetcode/problems/power_of_two/solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int base=2;
-    
-    for(int i=0;i<31;i++){
-        if(pow(base,i)==n){
-            return true;
-            break;
+        int base = 2;
+        for (int i = 0; i < 31; i++) {
+            if (pow(base, i) == n) {
+                return true;
+            }
         }
-    }
-    return false;
+        return false;
     }
 };
