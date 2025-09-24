8f8b6ceb0c79cfec7fff3a935b198476de75b1e7
Veer Modi <veer.modi.cg@gmail.com>
2025-09-24 09:09:53 +0000

    Sync LeetCode submission - Reverse String (cpp)

--- leetcode/problems/reverse_string/solution.cpp
+++ leetcode/problems/reverse_string/solution.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int left = 0;
-        int right = s.size() - 1;
-
-        while (left < right) {
-            s[left] = s[left] + s[right];
-            s[right] = s[left] - s[right];
-            s[left] = s[left] - s[right];
+        int right = s.size() -1;
+        while(left <right){
+            swap(s[left],s[right]);
             left++;
             right--;
         }
