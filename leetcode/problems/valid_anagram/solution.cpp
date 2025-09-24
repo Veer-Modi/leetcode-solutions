247e3bf7a88026f54256e457cd17bd6d06086360
Veer Modi <veer.modi.cg@gmail.com>
2025-09-24 10:29:47 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- leetcode/problems/valid_anagram/solution.cpp
+++ leetcode/problems/valid_anagram/solution.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // if (s.size() != t.size())
-        //     return false;
-        // map<char, int> freq;
-        // for (int i = 0; i < s.length(); i++) {
-        //     freq[s[i]]++;
-        // }
-        // for (int i = 0; i < t.length(); i++) {
-        //     freq[t[i]]--;
-        //     if (freq[t[i]] < 0) {
-        //         return false;
-        //     }
-        // }
-        // return true;
-
-        if (s.length() != t.length())
+        if (s.size() != t.size()) {
             return false;
-
+        }
         int count[26] = {0};
-
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.size(); i++) {
             count[s[i] - 'a']++;
             count[t[i] - 'a']--;
         }
-
         for (int i = 0; i < 26; i++) {
             if (count[i] != 0) {
                 return false;
