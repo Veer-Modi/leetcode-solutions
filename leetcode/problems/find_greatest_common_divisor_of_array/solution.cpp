d37eb2a3787bd64aa51fc76dc1872043498b1803
Veer Modi <veer.modi.cg@gmail.com>
2025-10-03 09:45:01 +0000

    Sync LeetCode submission - Find Greatest Common Divisor of Array (cpp)

--- leetcode/problems/find_greatest_common_divisor_of_array/solution.cpp
+++ leetcode/problems/find_greatest_common_divisor_of_array/solution.cpp
@@ -3,6 +3,6 @@ public:
     int findGCD(vector<int>& nums) {
         int max = *max_element(nums.begin(), nums.end());
         int min = *min_element(nums.begin(), nums.end());
-        return gcd(min, max);
+        return gcd(max, min);
     }
 };
