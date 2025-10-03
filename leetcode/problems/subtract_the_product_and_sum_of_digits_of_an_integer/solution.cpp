3bc2117f28bd781c67c0592a0541cce3a6d2c8aa
Veer Modi <veer.modi.cg@gmail.com>
2025-10-03 10:22:20 +0000

    Sync LeetCode submission - Subtract the Product and Sum of Digits of an Integer (cpp)

--- leetcode/problems/subtract_the_product_and_sum_of_digits_of_an_integer/solution.cpp
+++ leetcode/problems/subtract_the_product_and_sum_of_digits_of_an_integer/solution.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        int s=0,p=1;
-        while(n!=0){
-            int digit = n%10;
-            p*=digit;
-            s+=digit;
-            n/=10;
+        int s = 0, p = 1;
+        while (n != 0) {
+            int digit = n % 10;
+            s += digit;
+            p *= digit;
+            n = n / 10;
         }
-        int ans = p-s;
-        return ans;
+        int result = p - s;
+        return result;
     }
 };
