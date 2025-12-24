4d38ec4aaa4a973973e091e8c05680a0461127f3
Veer Modi <veer.modi.cg@gmail.com>
2025-12-24 11:44:42 +0000

    Sync LeetCode submission - Single Number (cpp)

---
+++ leetcode/problems/single_number/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       int temp = 0;
+       for(int num:nums){
+        temp ^= num;
+       }
+       return temp;
+    }
+};
