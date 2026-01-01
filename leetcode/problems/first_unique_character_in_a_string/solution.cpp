f9a72c51689a8173193f3a28687c09fed61c64c8
Veer Modi <veer.modi.cg@gmail.com>
2026-01-01 17:11:11 +0000

    Sync LeetCode submission - First Unique Character in a String (cpp)

---
+++ leetcode/problems/first_unique_character_in_a_string/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        if (s == "") {
+            return -1;
+        }
+        map<char, int> m;
+        for (char ch : s) {
+            m[ch]++;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
