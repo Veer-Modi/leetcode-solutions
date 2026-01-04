f9194d27b3323424cec43bda0e7c45efd0795387
Veer Modi <veer.modi.cg@gmail.com>
2026-01-04 13:54:20 +0000

    Sync LeetCode submission - Backspace String Compare (cpp)

---
+++ leetcode/problems/backspace_string_compare/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '#') {
+                if (i > 0) {
+                    s.erase(i - 1, 2);
+                    i -= 2;
+                } else {
+                    s.erase(i, 1);
+                    i -= 1;
+                }
+            }
+        }
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] == '#') {
+                if (i > 0) {
+                    t.erase(i - 1, 2);
+                    i -= 2;
+                } else {
+                    t.erase(i, 1);
+                    i -= 1;
+                }
+            }
+        }
+        return s == t ? true : false;
+    }
+};
