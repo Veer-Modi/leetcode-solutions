87cf78932ba102d905c466e45c9e36376cb3f746
Veer Modi <veer.modi.cg@gmail.com>
2026-01-04 04:33:17 +0000

    Sync LeetCode submission - Four Divisors (cpp)

---
+++ leetcode/problems/four_divisors/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int totalSum = 0;
+
+        for (int n : nums) {
+            vector<int> divisors;
+            for (int i = 1; i * i <= n; i++) {
+                if (n % i == 0) {
+                    divisors.push_back(i);
+                    if (i != n / i) {
+                        divisors.push_back(n / i);
+                    }
+                }
+                if (divisors.size() > 4) {
+                    break;
+                }
+            }
+            if (divisors.size() == 4) {
+                for (int x : divisors) {
+                    totalSum += x;
+                }
+            }
+        }
+        return totalSum;
+    }
+};
